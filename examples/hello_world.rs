138c147ae30e97135808db06230f395e3d934e28
g2xpf <nameko.jack@gmail.com>
2019-09-11 15:11:02 +0900

    enabled vsync

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -3,12 +3,12 @@ extern crate glium_text;
 extern crate cgmath;
 
 use glium::Surface;
-use glium::glutin::{ControlFlow, Event, self, WindowEvent};
+use glium::glutin::{Event, self, WindowEvent};
 
 fn main() {
     let mut event_loop = glutin::EventsLoop::new();
     let window = glutin::WindowBuilder::new().with_dimensions((1024, 768).into());
-    let context = glutin::ContextBuilder::new();
+    let context = glutin::ContextBuilder::new().with_vsync(true);
     let display = glium::Display::new(window, context, &event_loop).unwrap();
     let system = glium_text::TextSystem::new(&display);
 
@@ -18,24 +18,27 @@ fn main() {
     let text_width = text.get_width();
     println!("Text width: {:?}", text_width);
 
-    event_loop.run_forever(|event| {
-        let (w, h) = display.get_framebuffer_dimensions();
+    let mut window_should_closed = false;
 
-        let matrix:[[f32; 4]; 4] = cgmath::Matrix4::new(
-            2.0 / text_width, 0.0, 0.0, 0.0,
-            0.0, 2.0 * (w as f32) / (h as f32) / text_width, 0.0, 0.0,
-            0.0, 0.0, 1.0, 0.0,
-            -1.0, -1.0, 0.0, 1.0f32,
+    let (w, h) = display.get_framebuffer_dimensions();
+
+    let matrix:[[f32; 4]; 4] = cgmath::Matrix4::new(
+        2.0 / text_width, 0.0, 0.0, 0.0,
+        0.0, 2.0 * (w as f32) / (h as f32) / text_width, 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0, 1.0f32,
         ).into();
+
+    while !window_should_closed {
 
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 0.0, 1.0);
         glium_text::draw(&text, &system, &mut target, matrix, (1.0, 1.0, 0.0, 1.0));
         target.finish().unwrap();
 
-        match event {
-            Event::WindowEvent {event: WindowEvent::CloseRequested, ..} => ControlFlow::Break,
-            _ => ControlFlow::Continue,
-        }
-    });
+        event_loop.poll_events(|event| match event {
+            Event::WindowEvent {event: WindowEvent::CloseRequested, ..} => window_should_closed = true,
+            _ => (),
+        });
+    }
 }
--- examples/user_text.rs
+++ examples/user_text.rs
@@ -4,14 +4,14 @@ extern crate cgmath;
 
 use std::path::Path;
 use glium::Surface;
-use glium::glutin::{ControlFlow, Event, self, WindowEvent};
+use glium::glutin::{Event, self, WindowEvent};
 
 fn main() {
     use std::fs::File;
 
     let mut event_loop = glutin::EventsLoop::new();
     let window = glutin::WindowBuilder::new().with_dimensions((1024, 768).into());
-    let context = glutin::ContextBuilder::new();
+    let context = glutin::ContextBuilder::new().with_vsync(true);
     let display = glium::Display::new(window, context, &event_loop).unwrap();
     let system = glium_text::TextSystem::new(&display);
 
@@ -29,32 +29,35 @@ fn main() {
 
     println!("Type with your keyboard");
 
-    event_loop.run_forever(|event| {
+    let (w, h) = display.get_framebuffer_dimensions();
+
+    let matrix:[[f32; 4]; 4] = cgmath::Matrix4::new(
+        0.1, 0.0, 0.0, 0.0,
+        0.0, 0.1 * (w as f32) / (h as f32), 0.0, 0.0,
+        0.0, 0.0, 1.0, 0.0,
+        -0.9, 0.0, 0.0, 1.0f32,
+    ).into();
+
+    let mut window_should_closed = false;
+
+    while !window_should_closed {
         let text = glium_text::TextDisplay::new(&system, &font, &buffer);
-
-        let (w, h) = display.get_framebuffer_dimensions();
-
-        let matrix:[[f32; 4]; 4] = cgmath::Matrix4::new(
-            0.1, 0.0, 0.0, 0.0,
-            0.0, 0.1 * (w as f32) / (h as f32), 0.0, 0.0,
-            0.0, 0.0, 1.0, 0.0,
-            -0.9, 0.0, 0.0, 1.0f32,
-        ).into();
 
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 0.0, 1.0);
         glium_text::draw(&text, &system, &mut target, matrix, (1.0, 1.0, 0.0, 1.0));
         target.finish().unwrap();
 
-        if let Event::WindowEvent {event, ..} = event {
-            match event {
-                WindowEvent::ReceivedCharacter('\r') => buffer.clear(),
-                WindowEvent::ReceivedCharacter(c) if c as u32 == 8 => { buffer.pop(); },
-                WindowEvent::ReceivedCharacter(chr) => buffer.push(chr),
-                WindowEvent::CloseRequested => return ControlFlow::Break,
-                _ => ()
+        event_loop.poll_events(|event| {
+            if let Event::WindowEvent { event, .. } = event {
+                match event {
+                    WindowEvent::ReceivedCharacter('\r') => buffer.clear(),
+                    WindowEvent::ReceivedCharacter(c) if c as u32 == 8 => { buffer.pop(); },
+                    WindowEvent::ReceivedCharacter(chr) => buffer.push(chr),
+                    WindowEvent::CloseRequested => window_should_closed = true,
+                    _ => ()
+                }
             }
-        }
-        ControlFlow::Continue
-    });
+        });
+    }
 }
